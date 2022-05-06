a859bec56d6faa8ac35bda278fce2109397aafa3
PaolaDaft <andrepaolagd@gmail.com>
2022-05-06 07:26:55 -0500

    Agregando nueva route /explorersInNode y Regresando un objeto

--- app.js
+++ app.js
@@ -16,6 +16,13 @@ app.get('/launchx', (req, res) => {
     res.send('Bienvenidos a launch X')
 })
 
+// Regresando un objeto 
+// localhost:300/explorersInNode
+app.get('/explorersInNode', (req, res) => {
+    const explorer = {name: "Explorer", msg: "Hello"}
+    res.send(explorer)
+})
+
 // Con esto inicializamos esta app
 app.listen(port, () =>{
     console.log(`Example app listening on port ${port}`)
